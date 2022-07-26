e272e3811bce9307a1f1466419bfaf6a3ae8175b
Icaro Castelo <icaro.castelo@boavista.local>
2022-07-26 11:52:24 -0300

    commit pra adicionar os tipos de pokwmon

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pokemonImage = document.querySelector('.pokemon_img_data');
-const pokemonNumber = document.querySelector('.pokemon_number_data')
-const pokemonName = document.querySelector('.pokemon_name_data')
+const pokemonNumber = document.querySelector('.pokemon_number_data');
+const pokemonName = document.querySelector('.pokemon_name_data');
+const pokemonType = document.querySelector('.type_pokemon');
 
 const form = document.querySelector('.busca');
 const search = document.querySelector('.search');
@@ -16,7 +17,12 @@ const fetchPokemon = async (pokemon) => {
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
     pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
+    if( data['types']['1'] == 0) {pokemonType.innerHTML = data['types']['0']['type']['name'] + " " + data['types']['1']['type']['name']}
+    else {pokemonType.innerHTML = data['types']['0']['type']['name']};
+    
+    if(data.id < 9) {pokemonNumber.innerHTML = "00" + data.id};
+    if(data.id > 9 && data.id<99) {pokemonNumber.innerHTML = "0" + data.id};
+    if(data.id > 99) {pokemonNumber.innerHTML = data.id};
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
 
