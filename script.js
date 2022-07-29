ee3f4351f8782bc19bb7d130eb7e79b6426849a1
Icaro Castelo <icaro.castelo@boavista.local>
2022-07-29 12:06:58 -0300

    commit do fim de semana

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const pokemonImage = document.querySelector('.pokemon_img_data');
 const pokemonNumber = document.querySelector('.pokemon_number_data');
 const pokemonName = document.querySelector('.pokemon_name_data');
-const pokemonType = document.querySelector('.type_pokemon');
+const pokemonType1 = document.querySelector('.type_pokemon1');
+const pokemonType2 = document.querySelector('.type_pokemon2');
 const pokemonHeight = document.querySelector('.height');
 
 const form = document.querySelector('.busca');
@@ -20,17 +21,35 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
 
-    if(data){
+    if (data) {
         pokemonName.innerHTML = data.name;
-        pokemonHeight.innerHTML = "Altura: "+ (data.height * 0.1) + "m";
-        if (data['types']['1'] == undefined){ pokemonType.innerHTML = "Tipo: " + data['types']['0']['type']['name'] } 
-            else  { pokemonType.innerHTML = "Tipo: " + data['types']['0']['type']['name'] + " / " + data['types']['1']['type']['name'] };
+        pokemonHeight.innerHTML = "Altura: " + (data.height * 0.1) + "m";
+
+        if (data['types']['1'] == undefined) {
+            pokemonType1.innerHTML = data['types']['0']['type']['name'];
+            pokemonType2.classList.remove('type_pokemon2');
+            pokemonType2.innerHTML = " ";
+        }
+        else {
+            pokemonType1.innerHTML = data['types']['0']['type']['name'];
+            pokemonType2.classList.add('type_pokemon2');
+            pokemonType2.innerHTML = data['types']['1']['type']['name'];
+        };
+
+        switch (data['types']['0']['type']['name']) {
+            case "grass": add.style.type_pokemon1.background-color:#a59;
+                break;
+            case "poison": pokemonType1.innerHTML = "EU TO DOIDO OU NÃO";
+                break;
+        };
+
+
         if (data.id < 9) { pokemonNumber.innerHTML = "00" + data.id };
         if (data.id > 9 && data.id < 99) { pokemonNumber.innerHTML = "0" + data.id };
         if (data.id > 99) { pokemonNumber.innerHTML = data.id };
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
-    } else { 
+    } else {
         pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not found :c';
         pokemonNumber.innerHTML = '';
